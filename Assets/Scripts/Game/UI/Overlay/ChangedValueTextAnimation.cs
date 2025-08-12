69f45258698ba706089202736199451ec0fe367b
2ev1l <xxx2ev1l@gmail.com>
2025-08-12 11:53:26 +0300

    Added animation on health decrease

--- Assets/Scripts/Game/UI/Overlay/ChangedValueTextAnimation.cs
+++ Assets/Scripts/Game/UI/Overlay/ChangedValueTextAnimation.cs
@@ -8,14 +8,23 @@ namespace Game.UI.Overlay
     {
         #region fields & properties
         [SerializeField] private FloatingTextFactory textFactory;
+        [SerializeField] private bool doOnIncrease = true;
+        [SerializeField] private bool doOnDecrease = true;
         #endregion fields & properties
 
         #region methods
         public void DoAnimation(int changedValue) => DoAnimation((float)changedValue);
         public void DoAnimation(float changedValue)
         {
+            if (!CanDoAnimation(changedValue)) return;
             string text = $"{(changedValue >= 0 ? "+" : "-")}{changedValue}";
             textFactory.SpawnText(text);
+        }
+        public bool CanDoAnimation(float changedValue)
+        {
+            if (doOnIncrease && changedValue > 0) return true;
+            if (doOnDecrease && changedValue < 0) return true;
+            return false;
         }
         #endregion methods
     }
